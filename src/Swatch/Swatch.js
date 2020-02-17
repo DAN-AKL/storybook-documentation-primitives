614c7a3930d46e909b576d7f44179a4bb599f894
Georgy Malanichev <georgy@malanichev.com>
2020-02-18 11:24:35 +1300

    fix(swatch): remove unused import

--- src/Swatch/Swatch.js
+++ src/Swatch/Swatch.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { styled } from '@storybook/theming';
-import { P } from '@storybook/components/html';
 import { withReset } from '@storybook/components/dist/typography/shared';
 import CopyToClipboard from 'react-copy-to-clipboard';
 
