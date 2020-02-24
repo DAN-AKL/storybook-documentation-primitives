65b4d3880320fa0644629f056c0c35150588a508
Georgy Malanichev <georgy@malanichev.com>
2020-02-25 10:51:30 +1300

    build(rollup): add json plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
 import external from 'rollup-plugin-peer-deps-external';
 
 import pkg from './package.json';
@@ -40,5 +41,6 @@ export default {
       runtimeHelpers: true,
     }),
     commonjs(),
+    json(),
   ],
 };
