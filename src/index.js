ac7a4955d4e198b6439c3814f57b2d21c52e5812
Georgy Malanichev <georgy@malanichev.com>
2020-02-26 14:12:54 +1300

    fix: add missing export for Swatches

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { Swatch, SwatchContainer } from './Swatch/Swatch';
+export { Swatch, Swatches } from './Swatch/Swatch';
 export { Usage, Correct, Incorrect } from './Usage/Usage';
 export { Info, Warning } from './Panel/Panel';
 export { SpacingScale, Row, Tile } from './SpacingScale/SpacingScale';
