f068bcebb9437aa1a15023c562a90279fc476ade
Georgy Malanichev <georgy@malanichev.com>
2020-07-06 16:59:34 +1200

    ci(github): replace auto with semantic-release

---
+++ release.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogTitle:
+          '# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines.',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        success: false,
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        // eslint-disable-next-line no-template-curly-in-string
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
+      },
+    ],
+  ],
+};
